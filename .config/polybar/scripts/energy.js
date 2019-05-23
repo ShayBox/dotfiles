974262e7c1125773115877f9cad43a0be62f16bb
Shayne Hartford (ShayBox) <shaybox@shaybox.com>
2019-05-23 17:14:51 -0400

    Updated files

--- .config/polybar/scripts/energy.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require('tplink-smarthome-api');
-
-new Client().getDevice({ host: '192.168.0.3' })
-	.then(async device => {
-		const stats = await device.emeter.getRealtime();
-		console.log(stats.current.toFixed(1));
-	})
-	.catch(() => 'Error');
